09ae8565b8715d7567798e12a46815f63eb40baf
DancingQuanta <andytheseeker@gmail.com>
2021-05-18 11:09:32 +0100

    Added dislay class for html repr

--- pydataanalysis/pandas_utils.py
+++ pydataanalysis/pandas_utils.py
@@ -15,6 +15,7 @@ class ShorthandFormatter(string.Formatter):
         # Special formatting for other types can be added here (floats, etc.)
         else:
             # Usual formatting:
+
             return super(ShorthandFormatter, self).format_field(
                 value, format_spec)
 
@@ -33,9 +34,27 @@ def _repr_latex_(self):
 
 def _repr_markdown_(self):
     import tabulatehelper as th
+
     return f"{th.md_table(self)}"
 
 print(pd.DataFrame._repr_latex_)
 pd.DataFrame._repr_latex_ = _repr_latex_  
 # pd.DataFrame._repr_markdown_ = _repr_markdown_
 # pd.DataFrame._repr_html_ = _repr_html_
+
+class display(object):
+    """Display HTML representation of multiple objects"""
+    # https://jakevdp.github.io/PythonDataScienceHandbook/03.08-aggregation-and-grouping.html
+    template = """<div style="float: left; padding: 10px;">
+    <p style='font-family:"Courier New", Courier, monospace'>{0}</p>{1}
+    </div>"""
+    def __init__(self, *args):
+        self.args = args
+        
+    def _repr_html_(self):
+        return '\n'.join(self.template.format(a, eval(a)._repr_html_())
+                         for a in self.args)
+    
+    def __repr__(self):
+        return '\n\n'.join(a + '\n' + repr(eval(a))
+                           for a in self.args)
